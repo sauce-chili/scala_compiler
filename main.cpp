e226e3d6497f7a6fc82a7119b4d76901eb8c0f59
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-11-06 23:43:17 +0300

    [FEAT] Живой начальный парсер без конфликтов

--- main.cpp
+++ main.cpp
@@ -1,12 +1,6 @@
-#include <iostream>
+extern int yyparse();
 
 int main() {
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        std::cout << "i = " << i << std::endl;
-    }
-
+    yyparse();
     return 0;
 }
