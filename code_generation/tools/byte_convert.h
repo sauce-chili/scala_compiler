750593b38ee1455dc4efd3aaa2d106ab718d6e91
Yurasov Roman <roman.yurasov.v@gmail.com>
2026-01-13 15:05:21 +0300

    feat: Добавлены вспомогательные элементы кодогенерации

---
+++ code_generation/tools/byte_convert.h
@@ -0,0 +1,16 @@
+#ifndef SCALA_BYTE_CONVERTATION_H
+#define COMPILER_BYTE_CONVERTATION_H
+
+#include <vector>
+#include <cstdint>
+#define u2(x)  x.begin() + 2, x.end()
+
+std::vector<char> IntToBytes(int value);
+
+std::vector<char> DoubleToBytes(double value);
+
+std::vector<char> Int16ToBytes(uint16_t value);
+
+std::vector<char> FloatToBytes(float value);
+
+#endif //SCALA_BYTE_CONVERTATION_H
---
+++ code_generation/tools/utils.h
@@ -0,0 +1,33 @@
+#ifndef SCALA_UTILS_H
+#define SCALA_UTILS_H
+#include <cstdint>
+#include "byte_convert.h"
+
+enum class AccessFlags : uint16_t
+{
+    Public = 0x0001,
+    Protected = 0x0004,
+    Private = 0x0002,
+    Static = 0x0008,
+    Final = 0x0010,
+    Super = 0x0020,
+    Abstract = 0x0400,
+};
+
+
+enum class ArrayType : uint8_t
+{
+    Boolean = 4,
+    Char = 5,
+    Float = 6,
+    Double = 7,
+    Byte = 8,
+    Short = 9,
+    Int = 10,
+    Long = 11
+};
+
+void merge(std::vector<char> &bytes, const std::vector<char> &buffer);
+void merge(std::vector<bool> &bytes, const std::vector<bool> &buffer);
+
+#endif //SCALA_UTILS_H
