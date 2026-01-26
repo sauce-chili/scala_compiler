72a233b9e90650ecc38b98dcb08cf280d142cb7b
dmitry-smirnov <saucechilidev@gmail.com>
2026-01-26 04:04:04 +0300

    feat: опционально включаемый логгер

---
+++ utils/logger.h
@@ -0,0 +1,22 @@
+//
+// Created by Dmitry Smirnov on 22.01.2026.
+//
+#pragma once
+
+#ifndef SCALA_LEXER_LOGGER_H
+#define SCALA_LEXER_LOGGER_H
+
+#include <iostream>
+
+#ifndef DEBUG_ENABLE
+    #define DEBUG_ENABLE 1 // 1 - включено, 0 - выключено
+#endif
+
+#if DEBUG_ENABLE
+    #define debug(x) std::cout << "[DEBUG] " << __FILE__ << ":" << __LINE__ << " > " << x << std::endl
+#else
+    // затычка, удалиться оптимизатором
+    #define debug(x) do {} while (0)
+#endif
+
+#endif //SCALA_LEXER_LOGGER_H
