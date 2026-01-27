7edd6fcca5450e48fe4d317de84281822f3d5f03
dmitry-smirnov <saucechilidev@gmail.com>
2026-01-27 16:56:21 +0300

    rename: подаваемый файл на вход

---
+++ input.scala
@@ -0,0 +1,24 @@
+// Test file for method call validation
+
+class Calculator {
+    def add(a: Int, b: Int): Int = a + b
+    def multiply(a: Int, b: Int): Int = a * b
+    def ++(a: Int): Int = a + 1
+}
+
+class MethodCallTest {
+    def testMethod(): Unit = {
+        val calc: Calculator = new Calculator()
+
+        // Valid call
+        val sum: Int = calc.add(1, 2)
+        
+        val s: Int = calc ++ 1
+
+        // Error: wrong argument types
+//        val result: Int = calc.add("a", "b")
+
+        // Error: method not found
+//        val x: Int = calc.subtract(5, 3)
+    }
+}
